2c4fb124d5451c978c3a369f09797f4047970ea7
mrudularavishankar-sangamone <mrudularavishankar-sangamone@gmail.com>
2025-12-29 09:49:45 +0530

    Dynamically displaying the days in the calendar

--- src/pages/calendar.jsx
+++ src/pages/calendar.jsx
@@ -4,6 +4,12 @@ export default function Calendar() {
   
   const daysOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
+  const currentDate = new Date();
+  const [currentMonth, setCurrentMonth] = useState(currentDate.getMonth());
+  const [currentYear, setCurrentYear] = useState(currentDate.getFullYear());
+  const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+  const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
+
   return (
     <div className = 'calendar-app'>
       <div className = 'calendar'> 
@@ -27,37 +33,10 @@ export default function Calendar() {
         </div>
 
         <div className = 'days'>
-          <span className = 'current-day'> 1 </span>
-          <span> 2 </span>
-          <span> 3 </span>
-          <span> 4 </span>
-          <span> 5 </span>
-          <span> 6 </span>
-          <span> 7 </span>
-          <span> 8 </span>
-          <span> 9 </span>
-          <span> 10 </span>
-          <span> 11 </span>
-          <span> 12 </span>
-          <span> 13 </span>
-          <span> 14 </span>
-          <span> 15 </span>
-          <span> 16 </span>
-          <span> 17 </span>
-          <span> 18 </span>
-          <span> 19 </span>
-          <span> 20 </span>
-          <span> 21 </span>
-          <span> 22 </span>
-          <span> 23 </span>
-          <span> 24 </span>
-          <span> 25 </span>
-          <span> 26 </span>
-          <span> 27 </span>
-          <span> 28 </span>
-          <span> 29 </span>
-          <span> 30 </span>
-          <span> 31 </span>
+          {[...Array(firstDayOfMonth).keys()].map((_, index) =>
+          (<span key = {`empty-${index}`}></span>))}
+          {[...Array(daysInMonth).keys()].map((day) => 
+          (<span key = {day + 1}> {day + 1} </span>))}
         </div>
 
       </div>
